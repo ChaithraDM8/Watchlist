1b753105e548155997c835c46495caf870e9030d
Chaithra <chaithradm8@gmail.com>
2022-12-04 21:06:59 +0530

    implimented

---
+++ src/main/java/com/driver/MovieRepository.java
@@ -0,0 +1,76 @@
+package com.driver;
+
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+@Component
+@Repository
+public class MovieRepository {
+
+    HashMap<String,Movie> movieDatabase =new HashMap<>();
+    HashMap<String,Director> directorDatabase =new HashMap<>();
+    HashMap<String,List<String>> movieDirectorDatabase =new HashMap<>();
+
+    List<String> listofMovies;
+    public void addMovie(Movie movie){
+        movieDatabase.put(movie.getName(),movie);
+
+    }
+
+    public void addDirector(Director director) {
+        directorDatabase.put(director.getName(),director);
+    }
+
+    public Movie getMovieByName(String name) {
+        return movieDatabase.get(name);
+    }
+
+    public Director getDirectorByName(String name) {
+        return directorDatabase.get(name);
+    }
+
+    public List<String> getAllMovies() {
+        listofMovies= new ArrayList<>();
+        for(Movie movies: movieDatabase.values()){
+            listofMovies.add(movies.getName());
+        }
+        return listofMovies;
+
+    }
+
+    public void addMovieDirectorPair(String movieName, String directorName) {
+        List<String> list = new ArrayList<>();
+        if (movieDirectorDatabase.containsKey(directorName)) {
+            list = movieDirectorDatabase.get(directorName);
+            list.add(movieName);
+            movieDirectorDatabase.put(directorName, list);
+        } else {
+            list.add(movieName);
+            movieDirectorDatabase.put(directorName, list);
+        }
+    }
+    public List<String> getMoviesByDirectorName(String directorName) {
+        return movieDirectorDatabase.get(directorName);
+    }
+
+    public void deleteDirectorByName(String directorName) {
+        List<String> list = new ArrayList<>();
+        list = movieDirectorDatabase.get(directorName);
+
+        movieDirectorDatabase.remove(directorName);
+        directorDatabase.remove(directorName);
+
+        for (String movieName : list) {
+            movieDatabase.remove(movieName);
+        }
+    }
+    public void deleteAllDirectors() {
+        for(String directorName: movieDirectorDatabase.keySet()){
+            deleteDirectorByName(directorName);
+        }
+    }
+}
---
+++ src/main/java/com/driver/MovieService.java
@@ -0,0 +1,51 @@
+package com.driver;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Component
+@Service
+public class MovieService {
+
+    @Autowired
+    MovieRepository movieRepository;
+
+    public void addMovies(Movie movie) {
+        movieRepository.addMovie(movie);
+    }
+
+    public void addDirector(Director director) {
+            movieRepository.addDirector(director);
+    }
+
+    public Movie getMovieByName(String name) {
+        return movieRepository.getMovieByName(name);
+    }
+
+    public Director getDirectorByName(String name) {
+        return movieRepository.getDirectorByName(name);
+    }
+
+    public List<String> getAllMovies() {
+        return movieRepository.getAllMovies();
+    }
+
+    public void addMovieDirectorPair(String movieName, String directorName) {
+        movieRepository.addMovieDirectorPair(movieName,directorName);
+    }
+
+    public List<String> getMoviesByDirectorName(String directorName) {
+        return movieRepository.getMoviesByDirectorName(directorName);
+    }
+
+    public void deleteDirectorByName(String directorName) {
+         movieRepository.deleteDirectorByName(directorName);
+    }
+
+    public void deleteAllDirectors() {
+        movieRepository.deleteAllDirectors();
+    }
+}
